72c6cd906b1f85a8070657baa25d0c371e20237d
Pieter Christy Yan Yudhistira <pieterchristyan7@gmail.com>
2025-10-16 18:40:28 +0700

    feat: no 2 globalscope

--- Coroutines/app/src/main/java/com/example/coroutines/QuoteViewModel.kt
+++ Coroutines/app/src/main/java/com/example/coroutines/QuoteViewModel.kt
@@ -2,6 +2,7 @@ package com.example.coroutines
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -11,7 +12,7 @@ class QuoteViewModel : ViewModel() {
     private val _quote = MutableStateFlow("Tap the button to get a quote")
     val quote: StateFlow<String> get() = _quote
     fun fetchQuote() {
-        viewModelScope.launch {
+        GlobalScope.launch {
             _quote.value = "Loading..."
             val result = repository.getRandomQuote()
             _quote.value = result
