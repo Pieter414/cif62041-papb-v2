e118515e6fcb1e974a7134c70afe969d3d89fee2
Pieter Christy Yan Yudhistira <pieterchristyan7@gmail.com>
2025-11-11 20:12:12 +0700

    feat: add filter button (active/done/all)

--- ToDoList/app/src/main/java/com/example/todolist/ui/theme/TodoScreen.kt
+++ ToDoList/app/src/main/java/com/example/todolist/ui/theme/TodoScreen.kt
@@ -3,17 +3,56 @@ package com.example.todolist.ui.theme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.todolist.viewmodel.TodoViewModel
+import com.example.todolist.model.FilterType
+
+@Composable
+fun FilterButton(
+    text: String,
+    type: FilterType,
+    current: FilterType,
+    onClick: () -> Unit
+) {
+    val isSelected = type == current
+
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(8.dp),
+        modifier = Modifier
+            .height(32.dp)
+            .padding(horizontal = 4.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor =
+                if (isSelected) MaterialTheme.colorScheme.primary
+                else Color(0xFF7A7680),
+            contentColor = Color.White
+        ),
+        contentPadding = PaddingValues(
+            horizontal = 32.dp,
+            vertical = 4.dp
+        )
+    ) {
+        Text(
+            text,
+            fontSize = 12.sp,
+        )
+    }
+}
 
 @Composable
 fun TodoScreen(vm: TodoViewModel = viewModel()) {
-    val todos by vm.todos.collectAsState()
+    val todos by vm.filteredTodos.collectAsState()
+    val filter by vm.filter.collectAsState()
+
     var text by rememberSaveable { mutableStateOf("") }
     Column(Modifier.padding(16.dp)) {
         OutlinedTextField(
@@ -31,6 +70,12 @@ fun TodoScreen(vm: TodoViewModel = viewModel()) {
             },
             modifier = Modifier.padding(vertical = 8.dp)
         ) { Text("Tambah") }
+        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+            FilterButton("All", FilterType.ALL, filter) { vm.setFilter(FilterType.ALL) }
+            FilterButton("Active", FilterType.ACTIVE, filter) { vm.setFilter(FilterType.ACTIVE) }
+            FilterButton("Done", FilterType.DONE, filter) { vm.setFilter(FilterType.DONE) }
+        }
+        Spacer(modifier = Modifier.height(8.dp))
         Divider()
         LazyColumn {
             items(todos) { todo ->
